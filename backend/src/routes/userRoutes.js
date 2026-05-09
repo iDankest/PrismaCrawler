6caed51f142b083f144e3c4012915b988b654494
Hades <hades.otero@outlook.es>
2026-05-09 17:55:43 +0100

    Bases rutas, middleware, contorladores, configuración y servicios con placeholder de IA

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -6,8 +6,15 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Definimos las rutas y les pasamos las funciones del controlador
+// Importamos el middleware
+const verifyToken = require('../middlewares/authMiddleware');
+
+// Rutas PÚBLICAS (No necesitan middleware, cualquiera puede entrar)
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+
+// Rutas PRIVADAS (Añadimos verifyToken ANTES del controlador)
+// Fíjate cómo va: Ruta -> Middleware -> Controlador
+router.get('/perfil', verifyToken, userController.getProfile); 
 
 module.exports = router;
