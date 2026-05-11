12e495924d8d52d4646216f84aeb9bc319a7d8d4
Kilian <dankestmirror@hotmail.es>
2026-05-11 14:07:04 +0100

    font add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Game from './pages/Game'
-import Header from './components/Header'
+/* import Header from './components/Header' */
 
 function App() {
   return (
     <Router>
-      <Header />
+{/*       <Header /> */}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/game" element={<Game />} />
